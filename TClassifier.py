e46aaebde0dff0d132557964451f12081b3ec464
pvargas <pvargas@odu.edu>
2017-09-30 15:16:31 -0400

    Incorporate Tutorial Code

--- TClassifier.py
+++ TClassifier.py
@@ -1,17 +1,76 @@
 import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 from time import strftime, localtime, time
 
+"""Build a twitter harassment detector model
+
+   This code uses line obtained from scikit-learn tutorial, specifically exercise 02 of 
+   the tutorial.  The tutorial url:
+        http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+        Author: Olivier Grisel <olivier.grisel@ensta.org>
+        License: Simplified BSD        
 """
-This Python program
-  1. takes as a command line argument twitter handle
-  2. extract tweets associated with handle
-"""
+
 __author__ = 'Plinio H. Vargas'
 __date__ = 'Thu,  Sep 21, 2017 at 09:46:01'
 __email__ = 'pvargas@cs.odu.edu'
 
 
-def classifier(filename):   #
+def classifier(harassment_data_folder):   #
+    # the training data folder must be passed as first argument
+    dataset = load_files(harassment_data_folder, shuffle=False)
+    print("n_samples: %d" % len(dataset.data))
+
+    # Split the dataset in training and test set:
+    docs_train, docs_test, y_train, y_test = train_test_split(
+        dataset.data, dataset.target, test_size=0.5)
+
+    # TASK: Build a vectorizer / classifier pipeline that filters out tokens
+    # that are too rare or too frequent
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+        ('clf', LinearSVC(C=1000)),
+    ])
+
+    # TASK: Build a grid search to find out whether unigrams or bigrams are
+    # more useful.
+    # Fit the pipeline on the training set using grid search for the parameters
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2)],
+    }
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
+    grid_search.fit(docs_train, y_train)
+
+    # TASK: print the mean and std for each candidate along with the parameter
+    # settings for all the candidates explored by grid search.
+    n_candidates = len(grid_search.cv_results_['params'])
+    for i in range(n_candidates):
+        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
+                 % (grid_search.cv_results_['params'][i],
+                    grid_search.cv_results_['mean_test_score'][i],
+                    grid_search.cv_results_['std_test_score'][i]))
+
+    # TASK: Predict the outcome on the testing set and store it in a variable
+    # named y_predicted
+    y_predicted = grid_search.predict(docs_test)
+
+    # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
+
+    # Print and plot the confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+
+    # import matplotlib.pyplot as plt
+    # plt.matshow(cm)
+    # plt.show()
 
     return
 
@@ -19,7 +78,7 @@ def classifier(filename):   #
 if __name__ == '__main__':
     # checks for argument
     if len(sys.argv) != 2:
-        print('Usage: python3 GetTweets.py <twitter-handle>')
+        print('Usage: python3 TClassifier.py <corpus_folder>')
         sys.exit(-1)
 
     # record running time
@@ -29,6 +88,7 @@ if __name__ == '__main__':
     # call extract_tweets
     crawled_pages = {}
     classifier(sys.argv[1])
+
     print('\nEnd Time:  %s' % strftime("%a,  %b %d, %Y at %H:%M:%S", localtime()))
     print('Execution Time: %.2f seconds' % (time()-start))
 sys.exit(0)
