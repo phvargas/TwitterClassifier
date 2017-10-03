6ba6cd211f3d314b44fda484375aff260d9971ac
pvargas <pvargas@odu.edu>
2017-10-03 16:01:47 -0400

    add Script

---
+++ StripData.py
@@ -0,0 +1,53 @@
+import sys
+from time import strftime, localtime, time
+import os, re
+
+"""
+This Python program
+  1. Strip specific content of Harassment Dataset 
+"""
+__author__ = 'Plinio H. Vargas'
+__date__ = 'Tue,  Oct 03, 2017 at 11:19:50'
+__email__ = 'pvargas@cs.odu.edu'
+
+
+def strip_data(action, in_file, out_file):
+    print("Stripping file: %s" % in_file)
+
+    counter = 0
+    if action == "handle":
+        with open(in_file, "r", encoding='iso-8859-1') as f_in:
+            with open(out_file, "w") as f_out:
+                for line in f_in:
+                    regex = re.findall("(@.*?)\s", line + " ")
+                    for value in regex:
+                        print('Replacing:', value, line.strip())
+                    counter += 1
+
+    return
+
+
+if __name__ == '__main__':
+    # record running time
+    start = time()
+    print('Starting Time: %s' % strftime("%a,  %b %d, %Y at %H:%M:%S", localtime()))
+
+    if len(sys.argv) != 4:
+        print('Usage: python3 StripData.py <Tco|handle> <input_filename> <output_filename')
+        sys.exit(-1)
+
+    elif sys.argv[1].lower() != "tco" and sys.argv[1].lower() != "handle":
+        print('\nOption MUST be Tco or handle')
+        print('Usage: python3 StripData.py <Tco|handle> <filename>')
+        sys.exit(-1)
+
+    elif not os.path.isfile(sys.argv[2]):
+        print('Could not find input file: %s' % sys.argv[2])
+        print('Usage: python3 StripData.py <Tco|handle> <filename>')
+        sys.exit(-1)
+
+    strip_data(sys.argv[1].lower(), sys.argv[2], sys.argv[3])
+
+    print('\nEnd Time:  %s' % strftime("%a,  %b %d, %Y at %H:%M:%S", localtime()))
+    print('Execution Time: %.2f seconds' % (time()-start))
+sys.exit(0)
