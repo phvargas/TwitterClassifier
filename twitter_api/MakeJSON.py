9b24952eb22062edfb71bf55e437b42ebbc5b1b0
pvargas <pvargas@odu.edu>
2017-10-18 22:50:52 -0400

    JSON object

---
+++ twitter_api/MakeJSON.py
@@ -0,0 +1,80 @@
+import sys
+import os
+import csv
+import json
+from time import strftime, localtime, time
+
+"""
+This Python program
+    converts csv text format into a json format
+"""
+__author__ = 'Plinio H. Vargas'
+__date__ = 'Wed,  Oct 18, 2017 at 20:22:53'
+__email__ = 'pvargas@cs.odu.edu'
+
+
+def make_json(filename):   # handler is twitter user name without @ example phone_dude for @phone_dude
+    """
+    Parameters used in make_json:
+       filename: path of filename where csv format resides
+    """
+
+    counter = 0
+    json_obj = []
+    json_title = []
+    with open(filename, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            counter += 1
+            json_row = {}
+
+            if counter == 1:
+                for title in row:
+                    json_title.append(title)
+            else:
+                idx = 0
+                for value in row:
+                    if json_title[idx] == 'name':
+                        json_row[json_title[idx]] = value
+                    else:
+                        json_row[json_title[idx]] = value.lower()
+
+                    idx += 1
+
+                json_obj.append(json_row)
+
+            print(', '.join(row))
+
+    print(json.dumps(json_obj, sort_keys=True, indent=4))
+    fhs = open('out', 'w')
+    json.dump(json_obj, fhs, sort_keys=True, indent=4)
+    fhs.close()
+
+    fhs = open('out', 'r')
+    test = json.load(fhs)
+
+    print(test)
+    return
+
+
+if __name__ == '__main__':
+    # record running time
+    start = time()
+    print('Starting Time: %s' % strftime("%a,  %b %d, %Y at %H:%M:%S", localtime()))
+
+    # checks for argument
+    if len(sys.argv) != 2:
+        print('Usage: python3 MakeJSON.py <filename>')
+        sys.exit(-1)
+
+    infile = sys.argv[1]
+    if not os.path.isfile(infile):
+        print('\nCould not find document: ' % infile)
+        print('Usage: python3 MakeJSON.py <filename>')
+        sys.exit(-1)
+
+    make_json(infile)
+
+    print('\nEnd Time:  %s' % strftime("%a,  %b %d, %Y at %H:%M:%S", localtime()))
+    print('Execution Time: %.2f seconds' % (time()-start))
+    sys.exit(0)
