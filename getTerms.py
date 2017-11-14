e35160d22fb4fe4e2a71f1d8d0020592755f1d99
pvargas <pvargas@odu.edu>
2017-11-14 14:27:37 -0500

    Update

--- getTerms.py
+++ getTerms.py
@@ -1,17 +1,33 @@
 import numpy as np
+import sys
+import os
 from sklearn.datasets import load_files
 from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 
-folder = 'data/dataset/no_ambiguous/no_harassment/'
+if len(sys.argv) < 2:
+    print('Usage: python3 getTerms.py <corpus_folder>')
+    sys.exit(-1)
+
+folder = sys.argv[1]
+if not os.path.isdir(folder):
+    print('Corpus path MUST be a directory')
+    print('Usage: python3 getTerms.py <corpus_folder>')
+    sys.exit(-1)
+
+corpus = folder.split("/")[-1]
+if not corpus:
+    corpus = folder.split("/")[-2]
+
+print('Loading corpus %s' % corpus)
 dataset = load_files(folder, shuffle=False)
 
-print(dataset.data)
-
+print('Removing stopwords....')
 stop_words = text.ENGLISH_STOP_WORDS.union(['https', 'http'])
-
 count_vect = CountVectorizer(analyzer='word', strip_accents='unicode', stop_words=stop_words)
 tfidf_transformer = TfidfTransformer()
+
+print('Transforming documents to TFIDF representation ...')
 X = count_vect.fit_transform(dataset.data)
 
 corpus_terms = list(zip(count_vect.get_feature_names(), np.asarray(X.sum(axis=0)).ravel()))
