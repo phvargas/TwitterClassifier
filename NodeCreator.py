5c58f15b61c42b8cf69ad5bf238c1b9350de148e
Plinio Vargas <plinio.vargas@verizon.net>
2018-01-31 07:50:31 -0500

    update

--- NodeCreator.py
+++ NodeCreator.py
@@ -1,7 +1,10 @@
 import sys
-from time import strftime, localtime, time
 import os
 import json
+import twitter
+from time import strftime, localtime, time
+from twitter_apps.Keys import provide_keys
+from twitter_apps.GetTweets import retrieve_tweets
 
 """
 This Python program
@@ -13,58 +16,71 @@ __email__ = 'pvargas@cs.odu.edu'
 
 
 def read_conversations(in_filename):
-    conv_dict = {}
     screen_dict = {}
     links = []
     nodes = []
     repliers_node = set()
+    deleted_accounts = []
+
+    # upload deleted accounts
+    with open('deleted_accounts.txt', mode='r') as fs_deleted:
+        for account in fs_deleted:
+            deleted_accounts.append(account.strip().lower())
 
     # load conversations
     with open(in_filename, "r", encoding='iso-8859-1') as fs:
         for record in fs:
             conv = json.loads(record.strip())
+            conv_dict = {'responds': []}
+            deleted_count = 0
 
             # gets the Twitter Conversation-ID from the thread
             if conv:
                 conv_idx = next(iter(conv.values()))['data-conversation-id']
-                conv_dict[conv_idx] = []
+                conv_dict['data-conversation-id'] = conv_idx
 
             for idx in conv:
                 print('{}: {}'.format(idx, conv[idx]))
 
-                # place conversations-info into a dictionary
-                conv_dict[conv_idx].append({'data-tweet-id': conv[idx]['data-tweet-id'],
-                                            'data-screen-name': conv[idx]['data-screen-name'],
-                                            'tweet-time': conv[idx]['tweet-time'],
-                                            'tweet-text': conv[idx]['tweet-text']
-                                            })
+                is_original_tweet = False
 
                 for key in conv[idx]:
                     print('\t{}: {}'.format(key, conv[idx][key]))
 
                     # check if conversation-id is the root
                     if key == 'data-conversation-id' and conv[idx][key] == idx:
-                        # add screen_name to conversation initiated by research group member
-                        if conv[idx]['data-screen-name'] in screen_dict:
-                            screen_dict[conv[idx]['data-screen-name']]['conv'].append(conv_idx)
-                        else:
-                            screen_dict[conv[idx]['data-screen-name']] = {}
-                            screen_dict[conv[idx]['data-screen-name']]['conv'] = [conv_idx]
+                        is_original_tweet = True
+                        break
 
-    for screen_name in screen_dict:
-        for screen_conv in screen_dict[screen_name]['conv']:
-            screen_dict[screen_name][screen_conv] = {}
-
-            for record in conv_dict[screen_conv]:
-                if record['data-tweet-id'] == screen_conv:
-                    screen_dict[screen_name][screen_conv]['tweet-text'] = record['tweet-text']
-                    screen_dict[screen_name][screen_conv]['tweet-time'] = record['tweet-time']
-
+                if is_original_tweet:
+                    conv_dict['data-screen-name'] = conv[idx]['data-screen-name']
+                    conv_dict['tweet-time'] = conv[idx]['tweet-time']
+                    conv_dict['tweet-text'] = conv[idx]['tweet-text']
                 else:
-                    if 'responds' in screen_dict[screen_name][screen_conv]:
-                        screen_dict[screen_name][screen_conv]['responds'].append(record)
-                    else:
-                        screen_dict[screen_name][screen_conv]['responds'] = [record]
+                    if conv[idx]['data-screen-name'].lower() in deleted_accounts:
+                        deleted_count += 1
+
+                    conv_dict['responds'].append({'data-tweet-id': conv[idx]['data-tweet-id'],
+                                                  'data-screen-name': conv[idx]['data-screen-name'],
+                                                  'tweet-time': conv[idx]['tweet-time'],
+                                                  'tweet-text': conv[idx]['tweet-text']
+                                                  })
+
+            if conv:
+                if conv_dict['data-screen-name'] not in screen_dict:
+                    screen_dict[conv_dict['data-screen-name']] = {conv_dict['data-conversation-id']: {
+                        'tweet-time': conv_dict['tweet-time'],
+                        'tweet-text': conv_dict['tweet-text'],
+                        'responds': conv_dict['responds'],
+                        'deleted-accounts': deleted_count
+                    }}
+                else:
+                    screen_dict[conv_dict['data-screen-name']][conv_dict['data-conversation-id']] = {
+                        'tweet-time': conv_dict['tweet-time'],
+                        'tweet-text': conv_dict['tweet-text'],
+                        'responds': conv_dict['responds'],
+                        'deleted-accounts': deleted_count
+                    }
 
     # add nodes
     nodes.append({"id": "0", "tweets": 0,
@@ -72,28 +88,31 @@ def read_conversations(in_filename):
                   })
     for screen_name in screen_dict:
         number_replies = 0
+        deleted_count = 0
+
         for conversation in screen_dict[screen_name]:
             conv_replies = 0
-            if conversation != 'conv':
-                if 'responds' in screen_dict[screen_name][conversation]:
-                    conv_replies = len(screen_dict[screen_name][conversation]['responds'])
-                    number_replies += conv_replies
 
-                    for edge in screen_dict[screen_name][conversation]['responds']:
-                        # links.append({"source": edge['data-screen-name'], "target": conversation,
-                        #               "tweet": edge['tweet-text']})
+            if 'responds' in screen_dict[screen_name][conversation]:
+                conv_replies = len(screen_dict[screen_name][conversation]['responds'])
+                number_replies += conv_replies
+                deleted_count += screen_dict[screen_name][conversation]['deleted-accounts']
 
-                        repliers_node.add(edge['data-screen-name'])
+                for edge in screen_dict[screen_name][conversation]['responds']:
+                    # links.append({"source": edge['data-screen-name'], "target": conversation,
+                    #               "tweet": edge['tweet-text']})
 
-                # conversations nodes
-                nodes.append({"id": conversation, "tweets": 1, "responses": conv_replies})
+                    repliers_node.add(edge['data-screen-name'])
 
-                links.append({"source": screen_name, "target": conversation,
-                              "tweet": screen_dict[screen_name][conversation]['tweet-text']})
+            # conversations nodes
+            nodes.append({"id": conversation, "tweets": 1, "responses": conv_replies})
+
+            links.append({"source": screen_name, "target": conversation,
+                          "tweet": screen_dict[screen_name][conversation]['tweet-text']})
 
         # nodes of research group conversations
-        nodes.append({"id": screen_name, "tweets": len(screen_dict[screen_name]) - 1,
-                      "responses": number_replies
+        nodes.append({"id": screen_name, "tweets": len(screen_dict[screen_name]),
+                      "responses": number_replies, 'deleted-accounts': deleted_count
                       })
 
         links.append({"source": "0", "target": screen_name,
@@ -107,6 +126,27 @@ def read_conversations(in_filename):
 
     print('Number of repliers nodes:', len(repliers_node))
     print('Number of repliers edges:', len(links))
+
+    """    
+    # find which nodes were deleted
+    key = provide_keys('males')
+    deleted_accounts = []
+    api = twitter.Api(consumer_key=key['consumer_key'],
+                      consumer_secret=key['consumer_secret'],
+                      access_token_key=key['access_token_key'],
+                      access_token_secret=key['access_token_secret'])
+
+    for node in repliers_node:
+        print(node)
+        if not retrieve_tweets(api, node, count=1):
+            print(node, 'was deleted...')
+            deleted_accounts.append(node)
+    
+    with open('deleted_accounts.txt', mode='w') as fs_deleted:
+        for node in deleted_accounts:
+            fs_deleted.write('{}\n'.format(node))
+
+    """
 
     # nodes of Twitter handles interacting with research group conversations
     """
@@ -123,7 +163,7 @@ def read_conversations(in_filename):
     json.dump(screen_dict, f_out, indent=4)
     f_out.close()
 
-    data_dict = { "nodes": nodes, "links": links }
+    data_dict = {"nodes": nodes, "links": links}
     f_out = open('force_data.json', mode='w')
     json.dump(data_dict, f_out, indent=4)
     f_out.close()
