566e6c69dfbbfa2b70e65c9f9b691cf79d6bc8cf
Plinio Vargas <plinio.vargas@verizon.net>
2018-02-12 06:01:34 -0500

    update

---
+++ R/ResponseInConversations.R
@@ -0,0 +1,76 @@
+conv_data <- read.csv("conversation.csv")
+
+conv_matrix <- data.matrix(conv_data[17:91])
+final_value <- as.data.frame(t(conv_matrix))
+colnames(final_value) <- conv_data$Handle
+
+liberal <- c("blue")
+conservative <- c("red")
+neutral <- c("black")
+unknown <- c("magenta")
+notapplicable <- c("black")
+
+get_color <- as.data.frame(t(data.frame(liberal, conservative, 
+                                        neutral, unknown, notapplicable)))
+colnames(get_color) <- "Color"
+
+element <- 0
+for (k in names(final_value)){
+  conv_vector <- final_value[[k]]
+
+  element <- element + 1
+  counter <- 0    # Number of Conversations for a particular handle
+  for (i in conv_vector) {  
+    counter <- counter + 1
+      if (is.na(i)) 
+        break
+    }
+
+  sort(conv_vector, decreasing = TRUE)
+
+  vtu <- k
+  
+  for (extractName in conv_data$Name[element]){
+    fullname <- extractName
+    break
+  }
+  
+  for (extractStance in conv_data$Stance[element]){
+    stance <- extractStance
+    break
+  }
+  
+  element_std <- conv_data$std[element]
+  
+  for (extractSex in conv_data$Sex[element]){
+    sex <- extractSex
+    break
+  }
+  
+  
+  if (tolower(sex) == 'female')
+    pitch_symbol <- 1
+  else
+    pitch_symbol <- 3
+
+  for (extractColor in get_color$Color[stance]){
+    symbol_color <- extractColor
+    break
+  }
+  
+  followers = format(conv_data$Followers[element], big.mark=",", scientific=FALSE)
+  
+  g_title <- paste("Tweets in ", fullname, "Conversations\n",
+                   "Sex=", sex, " Stance=", stance, 
+                   " STD=", element_std, "\n",
+                   "Response per Tweet=", conv_data$Ave.Response[element],
+                   " Followers=", followers)
+  print(g_title)
+  print(conv_vector)
+  print(symbol_color)
+  
+  plot(sort(conv_vector, decreasing = TRUE), col=symbol_color,
+       pch=pitch_symbol,
+       xlab="Conversation", ylab = "Number of Responses", main = g_title)
+}
+
