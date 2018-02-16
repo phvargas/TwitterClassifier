097e45fa685dc47b2f7a7c93be1e08b7e4b9bd08
Plinio Vargas <plinio.vargas@verizon.net>
2018-02-16 04:30:20 -0500

    update

--- R/histogram.R
+++ R/histogram.R
@@ -1,11 +1,32 @@
-freq_data <- read.csv("data/SeanHannity-freq-count.csv")
+freq_data <- read.csv("SeanHannity-freq-count.csv")
 
 fullname <- "Sean Hannity"
-
-g_title <- paste("Histogram of ", fullname, "\nAccounts Reappearance in Conversation")
-
-#hist(x = freq_data$Account_Appearance, ylim = freq_data$Freq,
-#     xlab="Account Repetition", ylab = "Frequency", main = g_title)
-
+g_title <- paste("Histogram of Repeated Accounts\n", "in ", fullname, " Conversations")
 barplot(freq_data$Freq, names.arg=freq_data$Account_Appearance, log="y", ylim=c(1, 5000), col="red",
-     ylab = "Frequency of Reappearance", xlab="Number of Accounts Reaperance", main = g_title)
+        ylab = "Frequency of Repetition", xlab="Number of Repeated Accounts", main = g_title)
+
+freq_data <- read.csv("HillaryClinton-freq-count.csv")
+
+fullname <- "Hillary Clinton"
+g_title <- paste("Histogram of Repeated Accounts\n", "in ", fullname, " Conversations")
+barplot(freq_data$Freq, names.arg=freq_data$Account_Appearance, log="y", ylim=c(1, 5000), col="blue",
+        ylab = "Frequency of Repetition", xlab="Number of Repeated Accounts", main = g_title)
+
+
+freq_data <- read.csv("BillKeller-freq-count.csv")
+
+fullname <- "Bill Keller"
+g_title <- paste("Histogram of Repeated Accounts\n", "in ", fullname, 
+                 " Conversations\n Unknown Political Stance")
+barplot(freq_data$Freq, names.arg=freq_data$Account_Appearance, ylim=c(0, 50), col="green",
+        ylab = "Frequency of Repetition", xlab="Number of Repeated Accounts", main = g_title)
+
+
+
+freq_data <- read.csv("all-freq-count.csv")
+fullname <- "All"
+g_title <- paste("Histogram of Repeated Accounts\n", "in ", fullname, " Conversations")
+
+barplot(freq_data$Freq, names.arg=freq_data$Account_Appearance, log="y", ylim=c(1, 1000000), col="gray",
+     ylab = "Frequency of Repetition", xlab="Number of Repeated Accounts", main = g_title)
+
